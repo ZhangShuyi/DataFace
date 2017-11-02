baaaf76ba28da9ae1de830470e2c2e961a523368
JumpingDoggy <zsywings@gmail.com>
2017-11-02 15:24:25 +0800

    Add Log function v1.0

--- Code/getpicture.py
+++ Code/getpicture.py
@@ -1,63 +1,58 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Sep 15 16:13:39 2017
-
-@author: DCS
+    Version 1.0
+    To fetch picture from Internet
+    Use data from ModelAndTxt/FacesToBeFetched.txt
 """
 
-#-*- coding:utf-8 -*-
 import os
 import re
 import requests
 
 
-file_path='faces.txt'
-pic_url=[]
+FACE_TXT_FILE = "ModelAndTxt/FacesToBeFetched.txt"
+PIC_URL_LIST = []
 
-file_object = open(file_path)
-i=1
-readnum=0
-startpos=2691
-try:
-    while(1):
-        line = file_object.readline( )
-        readnum+=1
-        if readnum<startpos:
-            continue
-        print("line:",readnum)
-        line_s=line.split()
-        if len(line_s)>3 and re.match(r'^https?:/{2}\w.+$', line_s[-3]):
-            print(line_s[-3],line_s[-4])
-            try:
-                pic= requests.get(line_s[-3], timeout=20)
-                path_string = 'datas\\'
-                if len(line_s)==6:
-                    path_string= path_string+line_s[-6]+line_s[-5]
-                elif len(line_s)==5:
-                    path_string= path_string+line_s[-5]
-                else:
-                    print("【错误】地址不合法")
-                    print (path_string)
+
+def getPictureFromInternet(txt_path):
+    file_object = open(txt_path)
+    i = 1
+    readnum = 0
+    startpos = 2691
+    try:
+        while (1):
+            line = file_object.readline()
+            readnum += 1
+            if readnum < startpos:
+                continue
+            print("line:", readnum)
+            line_s = line.split()
+            if len(line_s) > 3 and re.match(r'^https?:/{2}\w.+$', line_s[-3]):
+                print(line_s[-3], line_s[-4])
+                try:
+                    pic = requests.get(line_s[-3], timeout=20)
+                    path_string = 'datas\\'
+                    if len(line_s) == 6:
+                        path_string = path_string + line_s[-6] + line_s[-5]
+                    elif len(line_s) == 5:
+                        path_string = path_string + line_s[-5]
+                    else:
+                        print("Info: Address is inValid")
+                        print(path_string)
+                        continue
+
+                    if not os.path.exists(path_string):
+                        os.mkdir(path_string)
+                        i = 1
+                    fp = open(path_string + '\\' + line_s[-4] + '.jpg', 'wb')
+                    fp.write(pic.content)
+                    fp.close()
+                except requests.exceptions.ConnectionError:
+                    print('【错误】当前图片无法下载')
                     continue
-                
-                if not os.path.exists(path_string):
-                    os.mkdir(path_string)
-                    i=1
-                fp = open(path_string+'\\'+line_s[-4]+'.jpg','wb')
-                fp.write(pic.content)
-                fp.close()
-            except requests.exceptions.ConnectionError:
-                print ('【错误】当前图片无法下载')
-                continue
-            except requests.exceptions.TooManyRedirects:
-                print ('【错误】重定向太多')
-                continue
-             
-finally:
-     file_object.close( )
+                except requests.exceptions.TooManyRedirects:
+                    print('【错误】重定向太多')
+                    continue
 
-#html = requests.get(url).text
-#pic_url = re.findall('"objURL":"(.*?)",',html,re.S)
-
-       
-
+    finally:
+        file_object.close()
